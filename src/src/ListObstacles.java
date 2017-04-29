4cdfe99169359c7d15314dcae521c16ec226303b
eg538 <eg538@cornell.edu>
2017-04-29 15:08:10 -0400

    Modified setup for obstacles

---
+++ src/src/ListObstacles.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.ArrayList;
+
+public class ListObstacles {
+	ArrayList<Obstacle> obstacles;
+	
+	public ListObstacles(){
+		obstacles = new ArrayList<Obstacle>();
+	}
+}
---
+++ src/src/Obstacle.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Obstacle {
+	float xpos;
+	float ypos;
+	float height;
+	float width;
+	
+	public Obstacle(float x, float y, float h, float w) {
+		xpos = x;
+		ypos = y;
+		height = h;
+		width = w;
+	}
+}
